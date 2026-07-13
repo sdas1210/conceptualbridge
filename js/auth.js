be196036b4f8b54262a0209431db512fd6437d2e
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-07-13 22:29:45 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,28 @@
 import { auth } from "./firebase-config.js";
 
-console.log("✅ Firebase Connected");
-console.log(auth);
+import {
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const provider = new GoogleAuthProvider();
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+
+    try {
+
+        const result = await signInWithPopup(auth, provider);
+
+        console.log(result.user);
+
+        alert("Welcome " + result.user.displayName);
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert(error.message);
+
+    }
+
+});
