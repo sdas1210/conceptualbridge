16455d5df0f2296c3c40b7be308fe7787fc82f8b
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-07-26 20:03:53 +0530

    Update fetch-questions.js

--- api/fetch-questions.js
+++ api/fetch-questions.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import { parseQuestionFile } from "../services/questionParser.js";
+import { parseQuestionFile as parseGeneralQuestionFile } from "../services/questionParser.js";
+import { parseQuestionFile as parseMathQuestionFile } from "../services/mathParser.js";
 export default async function handler(req, res) {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
