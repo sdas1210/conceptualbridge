3fb295bbbbef0be00aa06772d355a6ff0556cdfc
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-07-28 21:04:37 +0530

    Update full-mock-status.js

--- api/full-mock-status.js
+++ api/full-mock-status.js
@@ -1,1 +1,5 @@
+import fs from "fs";
+import path from "path";
 
+import { parseQuestionFile as parseGeneralQuestionFile } from "../services/questionParser.js";
+import { parseQuestionFile as parseMathQuestionFile } from "../services/mathParser.js";
