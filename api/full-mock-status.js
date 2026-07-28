121ccaf2aa1a8459986a1617a0bc086a5ff99e7a
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-07-28 21:05:06 +0530

    Update full-mock-status.js

--- api/full-mock-status.js
+++ api/full-mock-status.js
@@ -3,3 +3,10 @@ import path from "path";
 
 import { parseQuestionFile as parseGeneralQuestionFile } from "../services/questionParser.js";
 import { parseQuestionFile as parseMathQuestionFile } from "../services/mathParser.js";
+
+export default async function handler(req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Content-Type", "application/json");
+
+}
