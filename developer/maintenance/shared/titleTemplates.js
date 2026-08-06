8dd43b7cf9b0cc56366ab242442407854beb69f5
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-08-07 02:57:21 +0530

    Create titleTemplates.js

---
+++ developer/maintenance/shared/titleTemplates.js
@@ -0,0 +1,30 @@
+/**
+ * Conceptual Bridge - Title Templates Store
+ * Subject-specific title templates.
+ */
+
+import { MODES } from '../constants.js';
+
+export const TITLE_TEMPLATES = Object.freeze({
+  [MODES.GACA]: [
+    {
+      id: 'gaca-live-100',
+      label: '100 Most Important GK/GA Live Question Analysis',
+      template: '100 Most Important GK/GA Live Question Analysis'
+    }
+  ],
+  [MODES.MATH]: [
+    {
+      id: 'math-live-analysis',
+      label: 'Complete Mathematics Chapter Live Analysis',
+      template: 'Complete Mathematics Chapter Live Analysis'
+    },
+    {
+      id: 'math-top-50',
+      label: '50 Most Important Quantitative Aptitude Questions',
+      template: '50 Most Important Quantitative Aptitude Questions'
+    }
+  ],
+  [MODES.GS]: [],
+  [MODES.GI]: []
+});
