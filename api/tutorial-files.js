0ea4ed3df815f9c350876e81fd5e6a94db2843e3
sdas1210 <80212350+sdas1210@users.noreply.github.com>
2026-08-13 21:08:26 +0530

    Update tutorial-files.js

--- api/tutorial-files.js
+++ api/tutorial-files.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     }
 
     const formattedSubject = subject.trim();
-    const allowedSubjects = ["GACA", "Math", "GI", "Science"];
+    const allowedSubjects = ["GACA", "MATH", "GI", "Science"];
 
     // 2. Validation: Check subject whitelist
     if (!allowedSubjects.includes(formattedSubject)) {
